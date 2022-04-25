af5a91ff8dd5e92b8ca1864b3779bcde5d133a65
2Kelvin <kvin.mwangi254@gmail.com>
2022-04-25 19:37:49 +0300

    added a link to my linkedIn profile

--- Components/Socials.js
+++ Components/Socials.js
@@ -7,9 +7,11 @@ import twitter from "./../icons/twitter.png";
 function Socials() {
     return (
         <div className="socialIcons">
+            {/* instagram icon */}
             <div><img src={insta} alt="Instagram Icon" /></div>
+            {/* twitter icon */}
             <div><img src={twitter} alt="Twitter Icon" /></div>
-            {/* to my LinkedIn profile */}
+            {/* linkedIn icon linked to my personal LinkedIn profile */}
             <div>
                 <a href="https://www.linkedin.com/in/kelvin-mwangi-722406217">
                 <img src={linkedIn} alt="LinkedIn Icon" />
